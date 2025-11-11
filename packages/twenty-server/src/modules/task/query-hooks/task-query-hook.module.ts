fe9a7a704c4e0e6f2c5be127c50d15a4ecb56708
Atul Mishra <mishrapintu8800@gmail.com>
2025-11-12 00:48:50 +0530

    Integrate UserRoleModule into task-query-hook.module.ts
    - Added UserRoleModule to the imports of task-query-hook.module.ts to enhance task query functionality with user role management.

--- packages/twenty-server/src/modules/task/query-hooks/task-query-hook.module.ts
+++ packages/twenty-server/src/modules/task/query-hooks/task-query-hook.module.ts
@@ -4,8 +4,10 @@ import { TaskFindManyPreQueryHook } from 'src/modules/task/query-hooks/task-find
 import { TaskFindOnePreQueryHook } from 'src/modules/task/query-hooks/task-find-one.pre-query.hook';
 import { TaskGroupByPreQueryHook } from 'src/modules/task/query-hooks/task-group-by.pre-query.hook';
 import { TaskVisibilityService } from 'src/modules/task/query-hooks/task-visibility.service';
+import { UserRoleModule } from 'src/engine/metadata-modules/user-role/user-role.module';
 
 @Module({
+  imports: [UserRoleModule],
   providers: [
     TaskFindManyPreQueryHook,
     TaskFindOnePreQueryHook,
